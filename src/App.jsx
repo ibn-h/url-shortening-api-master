0be914047cf23c893480f577d53e5c054e88c557
ibn-h <ibnnh.deen@gmail.com>
2026-01-31 17:12:37 +0100

    Add website components and basic header structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Benefits from "./components/benefits";
+import CTA from "./components/CTA";
+import Footer from "./components/footer";
+
 export default function App() {
-  return <div>test</div>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Benefits />
+      <CTA />
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="">
+      <div>
+        <a href="">
+          <img src="/public/logo.svg" alt="" />
+        </a>
+
+        <div>
+          
+        </div>
+      </div>
+    </header>
+  );
+}
