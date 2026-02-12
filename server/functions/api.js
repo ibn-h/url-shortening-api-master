c70c1ffaab766a944373a4b4b034763176acf88d
ibn-h <ibnnh.deen@gmail.com>
2026-02-12 09:56:17 +0100

    Set up API with netlify

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 
 const API_URL = "https://ulvis.net/api.php?url=";
-const PORT = 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const router = express.Router();
 
 app.get("/", (req, res) => {
   res.json({ message: "API is running" });
@@ -17,28 +19,18 @@ app.get("/shorten", async (req, res) => {
   try {
     const { url } = req.query;
 
-    if (!url) {
+    if (!url)
       return res.status(400).json({ error: "URL parameter is required" });
-    }
 
     const response = await fetch(API_URL + encodeURIComponent(url));
-
-    if (!response.ok) {
-      return res.status(response.status).json({
-        error: "Failed to shorten URL",
-      });
-    }
-
     const data = await response.text();
-    console.log("Response body:", data);
 
     res.json({ result: data });
   } catch (error) {
-    console.error("Error:", error);
     res.status(500).json({ error: "Server error: " + error.message });
   }
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
-});
+app.use("/.netlify/functional/api", router);
+
+export const handler = serverless(app);
