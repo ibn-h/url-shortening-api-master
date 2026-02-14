d13187b83ec1764439cc2be6d5de9f6d2f5ccfe2
ibn-h <ibnnh.deen@gmail.com>
2026-02-14 17:54:05 +0100

    Update paths again

--- src/components/benefits.jsx
+++ src/components/benefits.jsx
@@ -1,18 +1,18 @@
 const benefitsContent = [
   {
-    icon: "public/icon-brand-recognition.svg",
+    icon: "src/assets/icon-brand-recognition.svg",
     title: "Brand Recognition",
     description:
       "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.",
   },
   {
-    icon: "public/icon-detailed-records.svg",
+    icon: "src/assets/icon-detailed-records.svg",
     title: "Detailed Records",
     description:
       "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
   },
   {
-    icon: "public/icon-fully-customizable.svg",
+    icon: "src/assets/icon-fully-customizable.svg",
     title: "Fully Customizable",
     description:
       "Customize the appearance of your links to match your brand identity. Make your links stand out with a unique look.",
